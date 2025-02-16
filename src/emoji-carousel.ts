14105bb834b7e6171838e5c1866066f4d4cabcb0
Eryk Napierała <eryk.piast@gmail.com>
2025-02-16 16:27:38 +0100

    feat(ui): simulate scroll loop
    It's far from perfect, but more or less works.

--- src/emoji-carousel.ts
+++ src/emoji-carousel.ts
@@ -55,7 +55,9 @@ export function createEmojiCarousel(): () => void {
     .sort(() => Math.random() - 0.5)
     .forEach((emoji) => {
       const li = document.createElement("li");
-      li.textContent = emoji;
+      const span = document.createElement("span");
+      span.textContent = emoji;
+      li.appendChild(span);
       carouselList.prepend(li);
     });
 
@@ -64,12 +66,102 @@ export function createEmojiCarousel(): () => void {
     block: "end",
   });
 
+  const handleScrollEnd = () => {
+    const LOOP_ANIMATION_DELAY = 250;
+
+    // NOTE: The first handler triggers on the programatic scroll, and the second one on the user scroll
+    // that's why we need to nest handlers after each programatic scroll
+
+    // NOTE: while looping, we have to animate the span inside the li as changing the transform of the li itself while
+    // the scroll snap is active gives no effect - the emoji keeps its position in the center of the carousel
+    // window
+    carousel.addEventListener(
+      "scrollend",
+      function attachScrollEndHandler() {
+        carousel.addEventListener(
+          "scrollend",
+          () => {
+            const { firstElementChild, lastElementChild } = carouselList;
+
+            if (!firstElementChild || !lastElementChild) {
+              attachScrollEndHandler.call(this);
+              return;
+            }
+
+            if (firstElementChild && carousel.scrollTop === 0) {
+              const animation = firstElementChild.firstElementChild!.animate(
+                [
+                  { transform: "translateY(0)" },
+                  { transform: "translateY(100%)" },
+                ],
+                LOOP_ANIMATION_DELAY
+              );
+
+              animation.addEventListener(
+                "finish",
+                () => {
+                  lastElementChild!.scrollIntoView({
+                    behavior: "instant",
+                    block: "end",
+                  });
+
+                  handleScrollEnd();
+                },
+                { once: true }
+              );
+
+              return;
+            }
+
+            if (
+              lastElementChild &&
+              carousel.scrollTop >
+                carousel.scrollHeight - carousel.clientHeight - 5 // NOTE: 5 is a buffer for browser inconsistencies
+            ) {
+              const animation = lastElementChild.firstElementChild!.animate(
+                [
+                  { transform: "translateY(0)" },
+                  { transform: "translateY(-100%)" },
+                ],
+                LOOP_ANIMATION_DELAY
+              );
+
+              animation.addEventListener("progress", () => {
+                console.log("progress");
+              });
+
+              animation.addEventListener(
+                "finish",
+                () => {
+                  firstElementChild.scrollIntoView({
+                    behavior: "instant",
+                    block: "start",
+                  });
+
+                  handleScrollEnd();
+                },
+                { once: true }
+              );
+
+              return;
+            }
+
+            attachScrollEndHandler.call(this);
+          },
+          { once: true }
+        );
+      },
+      { once: true }
+    );
+  };
+
+  handleScrollEnd();
+
   return () => {
     const currentEmoji = document.elementFromPoint(
       carousel.offsetLeft + carousel.offsetWidth / 2,
       carousel.offsetTop + carousel.offsetHeight / 2
     );
-
     if (currentEmoji && currentEmoji.previousElementSibling) {
       (currentEmoji.previousElementSibling as HTMLElement).scrollIntoView({
         behavior: "smooth",
